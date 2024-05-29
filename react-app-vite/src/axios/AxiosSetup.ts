211e868a9325f6c90ab2664bc48bc7742f52c678
bhupendra <sunny13299@gmail.com>
2024-05-29 15:24:45 +0530

    need some changes in front end

--- react-app-vite/src/axios/AxiosSetup.ts
+++ react-app-vite/src/axios/AxiosSetup.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+
+
 let custom_axios = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
+    Authorization: "Bearer" + localStorage.getItem("token"),
     Accept: "*/*",
     "Content-Type": "application/json",
   },
